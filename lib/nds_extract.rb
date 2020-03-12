64e635eb70f3294e6745de65fbffe192b0898a08
Paul Ralyea <047par@gmail.com>
2020-03-12 05:05:16 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,6 +2,53 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
+   result = {}
+   director_index = 0
+  while director_index < nds.size do
+    director = nds[director_index]
+    result[director[:name]] = gross_for_director(director)
+    director_index += 1
+  end
+   result
+  end
+   
+   def gross_for_director(d)
+   total = 0
+   index = 0
+  while index < d[:movies].length do
+     total += d[:movies][index][:worldwide_gross]
+     index += 1
+   end
+
+   total
+ end
+ 
+ def list_of_directors(source)
+   director_index = 0
+   director_list = Array.new
+
+   while director_index < source.size do
+     director_list.push(source[director_index][:name])
+     director_index += 1
+   end
+   director_list
+ end
+ 
+ def total_gross(source)
+   total_gross_of_earnings = 0
+   director_index = 0
+
+   while director_index < source.size do
+     total_gross_of_earnings += gross_for_director( source[director_index] )
+     director_index += 1
+   end
+ total_gross_of_earnings
+ end
+ 
+  
+  
+  
+  
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
@@ -18,5 +65,5 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
-end
+
+
