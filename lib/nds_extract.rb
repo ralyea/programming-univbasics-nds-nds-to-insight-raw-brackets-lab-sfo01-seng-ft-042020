adc3847adac64eaf5554185ce45cb21d035fd8d0
Paul Ralyea <047par@gmail.com>
2020-03-12 05:01:54 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -24,7 +24,17 @@ def directors_totals(nds)
  end
  
  def list_of_directors(source)
-   
+   director_index = 0
+   director_list = Array.new
+
+   while director_index < source.size do
+     director_list.push(source[director_index][:name])
+     director_index += 1
+   end
+   director_list
+ end
+ 
+ 
  
   
   
